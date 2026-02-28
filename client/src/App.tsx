219d7e5ebce2c9c5b26e2902951f3297bea9f910
bhawanshi dosi <d9799165054@gmail.com>
2026-02-28 10:24:24 +0530

    Fix vite config - remove root path causing blank screen issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider, useTheme } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
@@ -24,7 +23,6 @@ function App() {
     <ThemeProvider defaultTheme="dark" storageKey="portfolio-theme">
       <QueryClientProvider client={queryClient}>
         <TooltipProvider>
-          <Toaster />
           <div className="min-h-screen w-full">
             {/* Background element */}
             <div className={cn(
